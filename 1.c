e2f9aa669e41019329313ff1f48bbc773efdf2f8
chirag mahna <chiragmahna500@gmail.com>
2025-12-01 16:25:48 +0530

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,12 +1,34 @@
-//Q1: Write a program to input two numbers and display their sum
+// Q128: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
+
+/*
+Sample Test Cases:
+Input 1:
+File: text.txt (Content: Welcome to C language)
+Output 1:
+Vowels: 8
+Consonants: 10
+
+*/
 #include<stdio.h>
 int main(){
-    int a , b ;
-    printf("enter number 1 : ");
-    scanf("%d", & a );
-    printf("enter number 2 : ");
-    scanf("%d", & b );
-    int c = a+b;
-    printf("%d", c);
-    return 0 ; 
+    FILE*ptr = fopen("Text.txt","r") ; 
+    int count_vowel = 0 ; 
+    int count_consonent = 0 ; 
+    while(1){
+        int ch = fgetc(ptr) ; 
+        if(ch==EOF){
+            break ; 
+        }
+        if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'){
+            if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+                count_vowel ++ ; 
+            }
+            else{
+                count_consonent ++; 
+            }
+        }
+    }
+    printf("Count of vowel : %d\n",count_vowel) ; 
+    printf("Count of consonent: %d",count_consonent) ; 
+    fclose(ptr) ; 
 }
